c86f7cff46b7d52a52b6a1d927b6fc396a71bac9
Fiantso Harena <fiantsorav@gmail.com>
2024-11-11 13:07:21 +0300

    feat: new api router

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import path from 'path';
 import dotenv from 'dotenv';
+import apiRouter from './api';
 
 dotenv.config();
 
@@ -8,6 +9,8 @@ const app = express();
 const port = process.env.PORT ?? 8080;
 
 app.use(express.static(path.resolve('src/public')));
+
+app.use('/api', apiRouter);
 
 app.get('/', (req, res) => {
   res.sendFile(path.resolve('src/public/pages/report/index.html'));
