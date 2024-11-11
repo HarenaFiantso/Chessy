7cb2877e2a731c035491ea91d103f80066ecc543
Fiantso Harena <fiantsorav@gmail.com>
2024-11-11 13:02:56 +0300

    chore: change default port

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import express from 'express';
 import path from 'path';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT ?? 8080;
@@ -10,6 +13,6 @@ app.get('/', (req, res) => {
   res.sendFile(path.resolve('src/public/pages/report/index.html'));
 });
 
-app.listen(port, () => {
+app.listen(process.env.PORT, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
